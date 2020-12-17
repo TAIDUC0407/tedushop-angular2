006c89cb2450bb9155c7cadedccdd51e269a85e6
TaiNguyen <nguyenductai0407@gmail.com>
2020-12-17 22:47:42 +0700

    routing for tedushop angular2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren:()=> import('./login/login.module').then(mod=>mod.LoginModule) },
+  { path: 'main', loadChildren: ()=> import('./main/main.module').then(mod=>mod.MainModule) }
+];
