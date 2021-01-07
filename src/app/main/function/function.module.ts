0c12621178b10b319f0eb85f119166c33980fef0
TaiNguyen <nguyenductai0407@gmail.com>
2021-01-07 21:02:10 +0700

    hiển thị danh sách func với treeview

--- src/app/main/function/function.module.ts
+++ src/app/main/function/function.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FunctionComponent } from './function.component';
-
-
+import { Routes,RouterModule } from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import { TreeModule } from '@circlon/angular-tree-component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+const functionRoutes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: FunctionComponent }
+];
 
 @NgModule({
   declarations: [FunctionComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TreeModule,
+    FormsModule,
+    ModalModule,
+    RouterModule.forChild(functionRoutes)
   ]
 })
 export class FunctionModule { }
--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -7,7 +7,8 @@ export const mainRoutes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       {path:'home', loadChildren:()=>import('./home/home.module').then(mod=>mod.HomeModule)},
       { path: 'user', loadChildren:()=>import('./user/user.module').then(mod=>mod.UserModule) },
-      { path: 'role', loadChildren:()=>import('./role/role.module').then(mod=>mod.RoleModule) }
+      { path: 'role', loadChildren:()=>import('./role/role.module').then(mod=>mod.RoleModule) },
+       { path: 'function', loadChildren:()=>import('./function/function.module').then(mod=>mod.FunctionModule) }
     ]
   }
 ];
